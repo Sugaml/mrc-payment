dedd4140203ad8cd5f2fbf86dd61996e788d39c0
babulalt <babulal.tamang@berrybytes.com>
2023-04-01 17:50:45 +0545

    preload student fixes

--- api/models/Transaction.go
+++ api/models/Transaction.go
@@ -8,8 +8,8 @@ type Transaction struct {
 	gorm.Model
 	Title       string   `gorm:"not null" json:"title"`
 	User        *User    `gorm:"foreignkey:UserID" json:"user"`
-	StudentID   uint     `gorm:"foreignkey:SID" json:"student_id"`
-	Student     *Student `gorm:"-" json:"student"`
+	StudentID   uint     `gorm:"foreignkey:StudentID" json:"student_id"`
+	Student     *Student `gorm:"student" json:"student"`
 	Amount      float64  `gorm:"not null" json:"amount"`
 	Invoice     *Invoice `gorm:"foreignkey:InvoiceID" json:"invoice"`
 	InvoiceID   uint     `gorm:"not null" json:"invoice_id"`
