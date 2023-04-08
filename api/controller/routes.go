7df595050aeeb8fe21a70b010a6e8a3f5dd97f12
babulalt <babulal.tamang@berrybytes.com>
2023-04-08 23:30:02 +0545

    fixes route

--- api/controller/routes.go
+++ api/controller/routes.go
@@ -23,7 +23,7 @@ func (server *Server) initializeRoutes() {
 	server.setJSON("/payment/verify", server.VerifyPayment, "POST")
 
 	server.setJSON("/payment/invoice", server.CreateGateway, "POST")
-	server.setAdmin("/payment/transactions", server.GetTransaction, "GET")
+	server.setJSON("/payment/transactions", server.GetTransaction, "GET")
 	server.setJSON("/payment/student/transactions", server.GetTransaction, "GET")
 
 	server.setJSON("payment/invoice", server.CreateIvoice, "POST")
